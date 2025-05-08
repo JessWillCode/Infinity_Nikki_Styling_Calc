ea94e7a41a5e1e4a26f489987bb449223f256fe7
Jess Williams <jesswillcode@gmail.com>
2025-05-07 22:09:27 -0400

    reconfigured styling to include daisyui as a tailwindcss plugin and changed the theme in the root layout

--- app/layout.js
+++ app/layout.js
@@ -3,9 +3,12 @@ import './globals.css'
 export default function RootLayout(props) {
   const { children } = props
   return (
-    <html suppressHydrationWarning>
+    <html suppressHydrationWarning data-theme='dracula'>
       <body>
+      {/*navbar*/}
+      {/*<div className='navbar '></div>*/}
         {children}
+      {/*footer*/}
       </body>
     </html>
   )
